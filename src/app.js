7d80a895fba38819607b7941a5ce8bd1b68b6c31
Elezer Master <megido17@gmail.com>
2021-08-20 15:35:38 +0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,2 +1,66 @@
 import './styles.css'
+import {ContextMenu} from './menu'
 
+
+const menu = document.querySelector(".menu");
+let menuVisible = false;
+
+const toggleMenu = command => {
+  menu.style.display = command === "show" ? "block" : "none";
+  menuVisible = !menuVisible;
+};
+
+const setPosition = ({ top, left }) => {
+  menu.style.left = `${left}px`;
+  menu.style.top = `${top}px`;
+  toggleMenu("show");
+};
+
+window.addEventListener("click", e => {
+  if (menuVisible) toggleMenu("hide");
+});
+
+menu.addEventListener("mouseover", e => {
+    //console.log("mouse-option", e.target.innerHTML);
+});
+
+menu.addEventListener("click", e => {
+  //console.log("li-option", e.target.innerHTML);
+  switch(e.target.innerHTML){
+    case 'Аналитика кликов':
+        console.log("Аналитика кликов");
+        const cont = new ContextMenu()
+        cont.open()
+        break;
+    case 'Случайная фигура':
+        console.log("Случайная фигура");
+        break;
+    case 'Таймер отсчета':
+        console.log("Таймер отсчета");
+        break;
+    case 'Случайный звук':
+        console.log("Случайный звук");
+        break;
+    case 'Случайный фон':
+        console.log("Случайный фон");
+        break;
+    case 'Кастомное сообщение':
+        console.log("Кастомное сообщение");
+        break;
+    case 'Собственный модуль':
+        console.log("Собственный модуль");
+        break;
+    default:
+        console.log("default");
+}
+});
+
+window.addEventListener("contextmenu", e => {
+  e.preventDefault();
+  const originPosition = {
+    left: e.pageX,
+    top: e.pageY
+  };
+  setPosition(originPosition);
+  return false;
+});
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,32 @@
 import {Menu} from './core/menu'
 
 export class ContextMenu extends Menu {
+    super(selector){
+        this.el = document.querySelector(selector)
+
+        document.body.addEventListener('click', event => {
+          console.log('event from extended menu', event)
+          if (event.target.offsetParent !== this.el) {
+            this.close()
+          }
+        })
+
+    }
+
+    open() {
+        console.log('open from extended menu')
+        //throw new Error(`"open" method should be implemented in Menu"`)
+      }
+    
+      close() {
+        console.log('close from extended menu')
+        //throw new Error(`"close" method should be implemented in Menu"`)
+      }
+    
+      add() {
+        console.log('add from extended menu')
+        //throw new Error(`"add" method should be implemented in Menu"`)
+      }
+
 
 }
