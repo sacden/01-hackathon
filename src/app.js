923466dc0735617ddb1e631d219bd8c89fb465cb
Elezer Master <megido17@gmail.com>
2021-08-22 19:01:24 +0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,66 +1,16 @@
-import './styles.css'
-import {ContextMenu} from './menu'
+import "./styles.css";
+import { ContextMenu } from "./menu";
+import { counterClick } from "./modules/clicks.module";
+import { counterClickvvv } from "./modules/shape.module";
+import {backgroundModule} from "./modules/background.module";
+import {weatherModule} from "./modules/weather.module";
 
+const menu = new ContextMenu();
 
-const menu = document.querySelector(".menu");
-let menuVisible = false;
+// console.log(menu);
+export default menu;
+const menuList = [counterClick, counterClickvvv, backgroundModule,weatherModule];
+// hi.render();
+menuList.forEach((el) => el.addItemInMenuList());
+menu.render();
 
-const toggleMenu = command => {
-  menu.style.display = command === "show" ? "block" : "none";
-  menuVisible = !menuVisible;
-};
-
-const setPosition = ({ top, left }) => {
-  menu.style.left = `${left}px`;
-  menu.style.top = `${top}px`;
-  toggleMenu("show");
-};
-
-window.addEventListener("click", e => {
-  if (menuVisible) toggleMenu("hide");
-});
-
-menu.addEventListener("mouseover", e => {
-    //console.log("mouse-option", e.target.innerHTML);
-});
-
-menu.addEventListener("click", e => {
-  //console.log("li-option", e.target.innerHTML);
-  switch(e.target.innerHTML){
-    case 'Аналитика кликов':
-        console.log("Аналитика кликов");
-        const cont = new ContextMenu()
-        cont.open()
-        break;
-    case 'Случайная фигура':
-        console.log("Случайная фигура");
-        break;
-    case 'Таймер отсчета':
-        console.log("Таймер отсчета");
-        break;
-    case 'Случайный звук':
-        console.log("Случайный звук");
-        break;
-    case 'Случайный фон':
-        console.log("Случайный фон");
-        break;
-    case 'Кастомное сообщение':
-        console.log("Кастомное сообщение");
-        break;
-    case 'Собственный модуль':
-        console.log("Собственный модуль");
-        break;
-    default:
-        console.log("default");
-}
-});
-
-window.addEventListener("contextmenu", e => {
-  e.preventDefault();
-  const originPosition = {
-    left: e.pageX,
-    top: e.pageY
-  };
-  setPosition(originPosition);
-  return false;
-});
--- src/menu.js
+++ src/menu.js
@@ -1,32 +1,75 @@
-import {Menu} from './core/menu'
+import { Menu } from "./core/menu";
+import { BackgroundModule } from "./modules/background.module";
 
 export class ContextMenu extends Menu {
-    super(selector){
-        this.el = document.querySelector(selector)
+  constructor(selector) {
+    super(selector);
 
-        document.body.addEventListener('click', event => {
-          console.log('event from extended menu', event)
-          if (event.target.offsetParent !== this.el) {
-            this.close()
-          }
-        })
+    this.modules = [
+      // "Включить слайдер",
+      // "Создать фигуру",
+      // "Кастомное сообщение",
+      // "Собственный модуль",
+    ];
+    // this.backgroundModule = new BackgroundModule();
+    this.add = this.add.bind(this);
 
+  }
+
+  close() {
+    document.getElementById("menu").style.display = "none";
+  }
+
+  open(e) {
+    e.preventDefault();
+    if (document.getElementById("menu").style.display != "block") {
+      let newmenu = document.getElementById("menu");
+      newmenu.style.display = "block";
+      newmenu.style.left = e.pageX + "px";
+      newmenu.style.top = e.pageY + "px";
     }
+    let newmenu = document.getElementById("menu");
+    //console.log(this.el);
+    newmenu.addEventListener("click", (e) => {
+      let fn = e.target.id;
 
-    open() {
-        console.log('open from extended menu')
-        //throw new Error(`"open" method should be implemented in Menu"`)
-      }
-    
-      close() {
-        console.log('close from extended menu')
-        //throw new Error(`"close" method should be implemented in Menu"`)
-      }
-    
-      add() {
-        console.log('add from extended menu')
-        //throw new Error(`"add" method should be implemented in Menu"`)
-      }
+      this.modules[fn].trigger();
+      // switch (fn) {
+      //   case "0":
+      //     // this.backgroundModule.render();
+      //     break;
+      //   case "1":
+      //     console.log(this.modules[1]);
+      //     break;
+      //   case "2":
+      //     console.log(this.modules[2]);
+      //     break;
+      //   case "3":
+      //     console.log(this.modules[3]);
+      //     break;
+      // }
+
+      // if (e.target.id === "0") {
+      // this.backgroundModule.render();
+      // }
+    });
+  }
+
+  add(text, trigger) {
+    console.log("add in menu", text, trigger);
+    this.modules = [...this.modules, { text, trigger }];
+  }
+  render() {
+    console.log("module", this.modules);
 
 
+    const getMenu = document.querySelector("#menu");
+    const getElements = this.modules.map(
+      (el, index) => `<li class="menu-item" id='${index}'>${el.text}</li>`
+    );
+    getMenu.insertAdjacentHTML("afterbegin", getElements.join(""));
+
+    document.onclick = this.close;
+    document.oncontextmenu = this.open.bind(this);
+  }
 }
